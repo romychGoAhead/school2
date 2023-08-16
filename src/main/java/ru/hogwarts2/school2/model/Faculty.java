44bec4733a0589c2a91ad1cfacfc1bf8bcf464fb
eremi <ereminr83@mail.ru>
2023-08-16 20:13:52 +0700

    four commit

--- src/main/java/ru/hogwarts2/school2/model/Faculty.java
+++ src/main/java/ru/hogwarts2/school2/model/Faculty.java
@@ -1,9 +1,9 @@
 package ru.hogwarts2.school2.model;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 @Entity
 public class Faculty {
